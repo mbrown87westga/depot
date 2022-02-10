818247a79d326dd9350640be915d726092b45129
theTrueMikeBrown <theTrueMikeBrown@gmail.com>
2022-02-10 18:53:48 -0500

    starting Chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_221614) do
+ActiveRecord::Schema.define(version: 2022_02_10_235002) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
