ad9d591700589c815efd3612960b8166c95b634b
mbrown87 <mbrown87@cs6242-vm11.uwg.westga.edu>
2022-02-11 09:40:24 -0500

    getting the tests working again. Working on g1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_112702) do
+ActiveRecord::Schema.define(version: 2022_02_11_134054) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
