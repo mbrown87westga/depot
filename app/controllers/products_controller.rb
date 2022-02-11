c1b105c73a9d124218733ad034637ce15bafe92a
mbrown87 <mbrown87@cs6242-vm11.uwg.westga.edu>
2022-02-11 06:21:39 -0500

    finishing chapter 11 - Added in web socket

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(:title)
   end
 
   # GET /products/1 or /products/1.json
@@ -39,33 +39,39 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.html { redirect_to @product,
+          notice: 'Product was successfully updated.' }
+          format.json { render :show, status: :ok, location: @product }
+
+          @products = Product.all.order(:title)
+          ActionCable.server.broadcast 'products',
+          html: render_to_string('store/index', layout: false)
+        else
+          format.html { render :edit }
+          format.json { render json: @product.errors,
+            status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # DELETE /products/1 or /products/1.json
+      def destroy
+        @product.destroy
+
+        respond_to do |format|
+          format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+          format.json { head :no_content }
+        end
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_product
+        @product = Product.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def product_params
+        params.require(:product).permit(:title, :description, :image_url, :price)
       end
     end
-  end
-
-  # DELETE /products/1 or /products/1.json
-  def destroy
-    @product.destroy
-
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
-    end
-end
