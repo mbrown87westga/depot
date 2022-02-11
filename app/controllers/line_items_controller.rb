8277547900c9eb47653cbd7c37edc34ed430d7d4
theTrueMikeBrown <theTrueMikeBrown@gmail.com>
2022-02-10 19:16:16 -0500

    finishing chapter 9. Cart added

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: %i[ show edit update destroy ]
 
   # GET /line_items or /line_items.json
@@ -21,50 +23,54 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
-        format.json { render :show, status: :created, location: @line_item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { redirect_to @line_item.cart,
+          notice: 'Line item was successfully created.' }
+          format.json { render :show,
+            status: :created, location: @line_item }
+          else
+            format.html { render :new }
+            format.json { render json: @line_item.errors,
+              status: :unprocessable_entity }
+            end
+          end
+        end
+
+        # PATCH/PUT /line_items/1 or /line_items/1.json
+        def update
+          respond_to do |format|
+            if @line_item.update(line_item_params)
+              format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
+              format.json { render :show, status: :ok, location: @line_item }
+            else
+              format.html { render :edit, status: :unprocessable_entity }
+              format.json { render json: @line_item.errors, status: :unprocessable_entity }
+            end
+          end
+        end
+
+        # DELETE /line_items/1 or /line_items/1.json
+        def destroy
+          @line_item.destroy
+
+          respond_to do |format|
+            format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+            format.json { head :no_content }
+          end
+        end
+
+        private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_line_item
+          @line_item = LineItem.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def line_item_params
+          params.require(:line_item).permit(:product_id, :cart_id)
+        end
       end
-    end
-  end
-
-  # PATCH/PUT /line_items/1 or /line_items/1.json
-  def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /line_items/1 or /line_items/1.json
-  def destroy
-    @line_item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
-end
