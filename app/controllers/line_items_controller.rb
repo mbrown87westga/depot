ea20a08711910920c7d832bed9d8887dff3437b7
mbrown87 <mbrown87@cs6242-vm11.uwg.westga.edu>
2022-02-11 05:30:14 -0500

    starting chapter 11 and finishing up F1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,48 +28,48 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
-          format.json { render :show,
-            status: :created, location: @line_item }
-          else
-            format.html { render :new }
-            format.json { render json: @line_item.errors,
-              status: :unprocessable_entity }
-            end
+        format.html { redirect_to store_index_url }
+        format.json { render :show,
+          status: :created, location: @line_item }
+        else
+          format.html { render :new }
+          format.json { render json: @line_item.errors,
+            status: :unprocessable_entity }
           end
-        end
-
-        # PATCH/PUT /line_items/1 or /line_items/1.json
-        def update
-          respond_to do |format|
-            if @line_item.update(line_item_params)
-              format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
-              format.json { render :show, status: :ok, location: @line_item }
-            else
-              format.html { render :edit, status: :unprocessable_entity }
-              format.json { render json: @line_item.errors, status: :unprocessable_entity }
-            end
-          end
-        end
-
-        # DELETE /line_items/1 or /line_items/1.json
-        def destroy
-          @line_item.destroy
-
-          respond_to do |format|
-            format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
-            format.json { head :no_content }
-          end
-        end
-
-        private
-        # Use callbacks to share common setup or constraints between actions.
-        def set_line_item
-          @line_item = LineItem.find(params[:id])
-        end
-
-        # Only allow a list of trusted parameters through.
-        def line_item_params
-          params.require(:line_item).permit(:product_id)
         end
       end
+
+      # PATCH/PUT /line_items/1 or /line_items/1.json
+      def update
+        respond_to do |format|
+          if @line_item.update(line_item_params)
+            format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
+            format.json { render :show, status: :ok, location: @line_item }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @line_item.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      # DELETE /line_items/1 or /line_items/1.json
+      def destroy
+        @line_item.destroy
+
+        respond_to do |format|
+          format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+          format.json { head :no_content }
+        end
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_line_item
+        @line_item = LineItem.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def line_item_params
+        params.require(:line_item).permit(:product_id)
+      end
+    end
