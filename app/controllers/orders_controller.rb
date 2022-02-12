56fcb9e3a349fc5830c2cf65f20087e78fcfb04c
mbrown87 <mbrown87@cs6242-vm13.uwg.westga.edu>
2022-02-11 22:00:53 -0500

    finishing up 14 - added payment processor and tested it

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,6 +24,7 @@ class OrdersController < ApplicationController
 
   # POST /orders or /orders.json
   def create
+
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
 
@@ -31,66 +32,68 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
         format.html { redirect_to store_index_url, notice:
           'Thank you for your order.' }
-          format.json { render :show, status: :created,
-            location: @order }
-          else
-            format.html { render :new }
-            format.json { render json: @order.errors,
-              status: :unprocessable_entity }
-            end
-          end
-        end
-
-        # PATCH/PUT /orders/1 or /orders/1.json
-        def update
-          respond_to do |format|
-            if @order.update(order_params)
-              format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
-              format.json { render :show, status: :ok, location: @order }
-            else
-              format.html { render :edit, status: :unprocessable_entity }
-              format.json { render json: @order.errors, status: :unprocessable_entity }
-            end
-          end
-        end
-
-        # DELETE /orders/1 or /orders/1.json
-        def destroy
-          @order.destroy
-
-          respond_to do |format|
-            format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
-            format.json { head :no_content }
-          end
-        end
-
-        def pay_type_params
-          if order_params[:pay_type] == "Credit card"
-            params.require(:order).permit(:credit_card_number, :expiration_date)
-          elsif order_params[:pay_type] == "Check"
-            params.require(:order).permit(:routing_number, :account_number)
-          elsif order_params[:pay_type] == "Purchase order"
-            params.require(:order).permit(:po_number)
-          else
-            {}
-          end
-        end
-
-        private
-        # Use callbacks to share common setup or constraints between actions.
-        def set_order
-          @order = Order.find(params[:id])
-        end
-
-        # Only allow a list of trusted parameters through.
-        def order_params
-          params.require(:order).permit(:name, :address, :email, :pay_type)
-        end
-        def ensure_cart_isnt_empty
-          if @cart.line_items.empty?
-            redirect_to store_index_url, notice: 'Your cart is empty'
-          end
-        end
+        format.json { render :show, status: :created,
+          location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors,
+          status: :unprocessable_entity }
       end
+    end
+  end
+
+  # PATCH/PUT /orders/1 or /orders/1.json
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /orders/1 or /orders/1.json
+  def destroy
+    @order.destroy
+
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:name, :address, :email, :pay_type)
+  end
+  def ensure_cart_isnt_empty
+    if @cart.line_items.empty?
+      redirect_to store_index_url, notice: 'Your cart is empty'
+    end
+  end
+
+  def pay_type_params
+    if order_params[:pay_type] == "Credit card"
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == "Purchase order"
+      params.require(:order).permit(:po_number)
+    else
+      {}
+    end
+  end
+
+end
