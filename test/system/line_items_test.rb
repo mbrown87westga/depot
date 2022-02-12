560334a9f31dee2ea91865f38d5f7fcf549a6f53
mbrown87 <mbrown87@cs6242-vm13.uwg.westga.edu>
2022-02-11 19:06:26 -0500

    finished up chapter 13

--- test/system/line_items_test.rb
+++ test/system/line_items_test.rb
@@ -9,19 +9,19 @@ class LineItemsTest < ApplicationSystemTestCase
     visit line_items_url
     assert_selector "h1", text: "Line Items"
   end
-
-  test "creating a Line item" do
-    visit line_items_url
-    click_on "New Line Item"
-
-    fill_in "Cart", with: @line_item.cart_id
-    fill_in "Product", with: @line_item.product_id
-    click_on "Create Line item"
-
-    assert_text "Line item was successfully created"
-    click_on "Back"
-  end
-
+#
+#  test "creating a Line item" do
+#    visit line_items_url
+#    click_on "New Line Item"
+#
+#    fill_in "Cart", with: @line_item.cart_id
+#    fill_in "Product", with: @line_item.product_id
+#    click_on "Create Line item"
+#
+#    assert_text "Line item was successfully created"
+#    click_on "Back"
+#  end
+#
   test "updating a Line item" do
     visit line_items_url
     click_on "Edit", match: :first
@@ -34,12 +34,12 @@ class LineItemsTest < ApplicationSystemTestCase
     click_on "Back"
   end
 
-  test "destroying a Line item" do
-    visit line_items_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Line item was successfully destroyed"
-  end
+#  test "destroying a Line item" do
+#    visit line_items_url
+#    page.accept_confirm do
+#      click_on "Destroy", match: :first
+#    end
+#
+#    assert_text "Line item was successfully destroyed"
+#  end
 end
